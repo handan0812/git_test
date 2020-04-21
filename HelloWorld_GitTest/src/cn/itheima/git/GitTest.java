8725f74c6de2ec49da0a750b1e8952d04c89901c
itcast <bos@itcast.cn>
2020-04-21 13:21:34 +0800

    提交并push到远程

--- HelloWorld_GitTest/src/cn/itheima/git/GitTest.java
+++ HelloWorld_GitTest/src/cn/itheima/git/GitTest.java
@@ -10,9 +10,11 @@ public class GitTest {
         System.out.println("Hello Git......");
         System.out.println("Hello Git......");
         System.out.println("Hello Git......");
+        System.out.println("hello  azhe  git");
     }
 
     public void test(){
         System.out.println("git nihao!!!");
+        System.out.println("git git git!!!");
     }
 }
