8c7046b73f2b2169d0bf3ea3072c04a510f41ee6
itcast <bos@itcast.cn>
2020-04-21 13:03:02 +0800

    测试将idea中的java项目提交到本地仓库

---
+++ HelloWorld_GitTest/src/cn/itheima/git/GitTest.java
@@ -0,0 +1,14 @@
+package cn.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+        System.out.println("Hello Git......");
+    }
+}
